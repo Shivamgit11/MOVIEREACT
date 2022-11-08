199aea24e5b803782ffb346c431cffd4bbb621fd
shivam <mailtoshivam2002@gmail.com>
2022-11-08 23:38:51 +0530

    5.10 working with post to firebase

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,9 +5,11 @@ import classes from './Movie.module.css';
 const Movie = (props) => {
   return (
     <li className={classes.movie}>
+      
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button className={classes.btn1}>DELETE MOVIE</button>
     </li>
   );
 };
